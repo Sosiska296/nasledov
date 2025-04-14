21084450971c215bc4cc57260c83737b5ddd0a28
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-04-14 19:52:07 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,18 @@
+﻿blitz three = new blitz();
+blitz four = new Loona();
+three.dad();
+four.dad();
+class blitz
+{
+    public virtual void dad()
+    {
+        Console.WriteLine("da");
+    }
+}
+class Loona : blitz
+{
+    public override void dad()
+    {
+        Console.WriteLine("po document");
+    }
+}
